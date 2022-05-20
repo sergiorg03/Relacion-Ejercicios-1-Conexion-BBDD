f4dbec5494c7e4afda1c64398d9831b2a1bc1dc0
serrodgen <srodgen0910@g.educaand.es>
2022-05-20 16:48:21 +0200

    Todos los ejercicios BBDD

--- src/main/java/com/mycompany/relacion_ejercicios1_bbdd/Ej12.java
+++ src/main/java/com/mycompany/relacion_ejercicios1_bbdd/Ej12.java
@@ -5,37 +5,40 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
-public class Ej11 {
+public class Ej12 {
 
     public static void main(String[] args) {
-    
+
         try {
-            
+
             String cadcon = "jdbc:mysql://localhost/empresa?serverTimezone=UTC";
             String user = "root";
             String password = "";
 
             Connection conexion = DriverManager.getConnection(cadcon, user, password);
-            
+
             Statement sentencia = conexion.createStatement();
 
-            ResultSet resultado = sentencia.executeQuery("SELECT count(numem),sum(salar) FROM empleados GROUP BY numde having numde in(SELECT distinct (numde) FROM empleados WHERE salar > 2500);");
-            
-            System.out.println("NUMERO DE EMPLEADOS\t\tSUMA DE SALARIOS");
-            System.out.println("--------------------\t\t-----------------");
-            
+            ResultSet resultado = sentencia.executeQuery("SELECT nomde, COUNT(numem), SUM(salar), SUM(comis), SUM(numhi) FROM empleados, departamentos WHERE direc = numem AND tidir = 'F' GROUP BY nomde;");
+
+            System.out.println("NOMDE\t\t\t\tCOUNT(NUMEM)\t\tSUM(SALAR)\t\tSUM(COMIS)\t\tSUM(NUMHI)");
+            System.out.println("----------------\t\t------------\t\t-----------\t\t-----------\t\t-----------");
+
             while (resultado.next()) {
-                
+
                 String nomem = resultado.getString(1);
-                int sum_salario = resultado.getInt(2);
-                
-                System.out.println(nomem +"\t\t\t\t"+ sum_salario);
+                int numem = resultado.getInt(2);
+                int sum_salar = resultado.getInt(3);
+                int sum_comis = resultado.getInt(4);
+                int sum_hijos = resultado.getInt(5);
+
+                System.out.println(nomem + "\t\t" + numem + "\t\t\t" + sum_salar + "\t\t\t" + sum_comis + "\t\t\t" + sum_hijos);
             }
-            
+
             resultado.close();
             sentencia.close();
             conexion.close();
-            
+
         } catch (Exception e) {
             e.printStackTrace();
         }
